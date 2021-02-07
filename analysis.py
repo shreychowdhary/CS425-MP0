30c5558301e52a3b487539480bf67a9559ebc102
shreychowdhary <shrey.chowdhary1@gmail.com>
2021-02-07 12:37:45 -0600

    Add anaylsis images

--- analysis.py
+++ analysis.py
@@ -8,7 +8,7 @@ median_vals = []
 max_vals = []
 min_vals = []
 ninety_vals = []
-for i in range(100):
+for i in range(101):
     between = df[df[0].between(i, i + 1)]
     median_vals.append(between[1].median())
     max_vals.append(between[1].max())
@@ -16,10 +16,11 @@ for i in range(100):
     ninety_vals.append(between[1].quantile(.9))
 
 fig1 = plt.figure(1)
-plt.plot(range(0, 100), median_vals)
-plt.plot(range(0, 100), max_vals)
-plt.plot(range(0, 100), min_vals)
-plt.plot(range(0, 100), ninety_vals)
+plt.rc('font', size=22)
+plt.plot(range(0, 101), median_vals)
+plt.plot(range(0, 101), max_vals)
+plt.plot(range(0, 101), min_vals)
+plt.plot(range(0, 101), ninety_vals)
 plt.title("Analysis of Logging Delay")
 plt.ylabel("Seconds")
 plt.xlabel("Time in seconds since first connection")
@@ -28,13 +29,14 @@ plt.legend(["Median Delay", "Max Delay", "Min Delay", "90% Delay"])
 df = pd.read_csv("3nodes.csv", header=None)
 df[0] = df[0] - df[0].min()
 kilobits_per_second = []
-for i in range(100):
+for i in range(101):
     between = df[df[0].between(i, i + 1)]
     kilobits_per_second.append(between[2].sum() * 8 / 1000)
 
 fig2 = plt.figure(2)
+plt.rc('font', size=22)
 plt.title("Analysis of Logging Bandwidth Usage")
 plt.ylabel("Kilobits per second")
 plt.xlabel("Time in seconds since first connection")
-plt.plot(range(0, 100), kilobits_per_second)
+plt.plot(range(0, 101), kilobits_per_second)
 plt.show()
